7e0efb79d34e577906a1445ab9959f8becc673b6
Danny Cheun <dcheun@gmail.com>
2025-07-25 19:50:05 -0700

    feat: Add endpoint that lists all the orders associated with a particular tag

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrdersByTagListView
 
 
 urlpatterns = [
     path("tags/", OrderTagListCreateView.as_view(), name="order-detail"),
+    path("tags/<int:tag_id>/orders/", OrdersByTagListView.as_view(), name="orders-by-tag"),
     path("", OrderListCreateView.as_view(), name="order-list"),
 ]
